5c5eab0605626371816852da2c268d136231b07b
Theodore Moreland <theodoremoreland@yahoo.com>
2020-12-27 03:27:23 -0600

    Views working

--- postgresql/views/v_anime_vs_manga.sql
+++ postgresql/views/v_anime_vs_manga.sql
@@ -1,8 +1,9 @@
 DROP VIEW IF EXISTS v_anime_vs_manga;
 CREATE or REPLACE VIEW v_anime_vs_manga AS
-  SELECT key, value, series_id, TO_DATE(answers ->> 'Week Ending', 'YYYY-MM-DD') as weekEnding FROM absences
-  CROSS JOIN LATERAL jsonb_each_text(answers)
+  SELECT key, value, enteredby_id, series_id, TO_DATE(review ->> 'Week Ending', 'YYYY-MM-DD') as weekEnding FROM reviews
+  CROSS JOIN LATERAL jsonb_each_text(review)
   WHERE
   	key IN (SELECT question FROM review_questions WHERE questiongroup = 'Anime Vs Manga')
-	AND dateentered IN (SELECT MAX(dateentered) FROM absences GROUP BY series_id, TO_DATE(answers ->> 'Week Ending', 'YYYY-MM-DD'))
-  ORDER BY series_id, weekEnding DESC, key;
+    AND dateentered IN (SELECT MAX(dateentered) FROM reviews GROUP BY enteredby_id, series_id, TO_DATE(review ->> 'Week Ending', 'YYYY-MM-DD'))
+  ORDER BY enteredby_id, series_id, weekEnding DESC, key;
+  
--- postgresql/views/v_expectations.sql
+++ postgresql/views/v_expectations.sql
@@ -1,9 +1,9 @@
-DROP VIEW IF EXISTS v_anime;
-CREATE or REPLACE VIEW v_anime AS
+DROP VIEW IF EXISTS v_expectations;
+CREATE or REPLACE VIEW v_expectations AS
   SELECT key, value, enteredby_id, series_id, TO_DATE(review ->> 'Week Ending', 'YYYY-MM-DD') as weekEnding FROM reviews
   CROSS JOIN LATERAL jsonb_each_text(review)
   WHERE
-  	key IN (SELECT question FROM review_questions WHERE questiongroup = 'Anime')
+  	key IN (SELECT question FROM review_questions WHERE questiongroup = 'Expectations')
     AND dateentered IN (SELECT MAX(dateentered) FROM reviews GROUP BY enteredby_id, series_id, TO_DATE(review ->> 'Week Ending', 'YYYY-MM-DD'))
   ORDER BY enteredby_id, series_id, weekEnding DESC, key;
   
