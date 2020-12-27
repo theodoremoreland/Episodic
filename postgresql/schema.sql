c9a2dc334f7808ea16448e3c857fdc25d3a87588
Theodore Moreland <theodoremoreland@yahoo.com>
2020-12-27 03:13:36 -0600

    Updated schema.sql

--- postgresql/schema.sql
+++ postgresql/schema.sql
@@ -1,66 +1,56 @@
-CREATE TABLE schools (
-   school_id  serial primary key,
-   district_id int,
-   name  varchar(100),
-   affiliation  varchar(100),
-   address  varchar(200),
-   grades  varchar(100),
-   sizestudentbody  int,
-   CONSTRAINT fk_district
-    FOREIGN KEY(district_id)
-        REFERENCES districts(district_id)
+CREATE TABLE review_questions (
+    question_id SERIAL,
+    question TEXT NOT NULL,
+  	inputType TEXT NOT NULL,
+  	questionGroup TEXT,
+  	hoverText TEXT,
+  	required BOOLEAN NOT NULL,
+    active BOOLEAN NOT NULL,
+    PRIMARY KEY ("question_id"),
+    UNIQUE (question, active)
 );
 
 
-CREATE TABLE users (
-   user_id  serial primary key,
-   district_id  int,
-   school_id  int,
+CREATE TABLE reviewers (
+   reviewer_id  serial primary key,
    username  VARCHAR(30),
    firstname  VARCHAR(50),
    lastname  VARCHAR(50),
-   emailaddress  VARCHAR(100),
-   phone VARCHAR(15),
-   CONSTRAINT fk_district
-    FOREIGN KEY(district_id)
-        REFERENCES districts(district_id),
-   CONSTRAINT fk_school
-    FOREIGN KEY(school_id)
-        REFERENCES schools(school_id)       
+   emailaddress  VARCHAR(100)     
 );
 
 
-CREATE TABLE absences(
+CREATE TABLE series (
+   series_id  serial primary key,
+   name  varchar(100),
+   studio  varchar(100),
+   genre varchar(100)
+);
+
+
+CREATE TABLE reviews(
    entry_id  serial primary key,
-   district_id  int,
-   school_id  int,
+   series_id  int,
    enteredby_id  int,
    dateentered  timestamp DEFAULT now(),
-   answers  jsonb,
-   CONSTRAINT fk_district
-    FOREIGN KEY(district_id)
-        REFERENCES districts(district_id),
-   CONSTRAINT fk_school
-    FOREIGN KEY(school_id)
-        REFERENCES schools(school_id),
-   CONSTRAINT fk_user
+   review  jsonb,
+   CONSTRAINT fk_series
+    FOREIGN KEY(series_id)
+        REFERENCES series(series_id),
+   CONSTRAINT fk_reviewer
     FOREIGN KEY(enteredby_id)
-        REFERENCES users(user_id)        
+        REFERENCES reviewers(reviewer_id)        
 );
 
 
-CREATE TABLE school_metadata(
-   "ID" serial,
-   school_id  int,
-   enteredby_id  int,
+CREATE TABLE reviewer_metadata(
+   metadata_id serial,
+   reviewer_id  int,
    dateentered  timestamp DEFAULT now(),
    metadata  json,
    active boolean,
-   PRIMARY KEY ("ID"),
-   CONSTRAINT fk_school
-    FOREIGN KEY(school_id)
-        REFERENCES schools(school_id),
-   CONSTRAINT fk_user
-    FOREIGN KEY(enteredby_id)
-        REFERENCES users(user_id)
+   PRIMARY KEY ("metadata_id"),
+   CONSTRAINT fk_reviewer
+    FOREIGN KEY(reviewer_id)
+        REFERENCES reviewers(reviewer_id)
 );
