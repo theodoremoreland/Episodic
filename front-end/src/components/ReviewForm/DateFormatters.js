5ad96a31631bc0c9264970fd75481966d963852a
Theodore Moreland <theodoremoreland@yahoo.com>
2026-04-06 18:53:15 -0500

    deleted odd front-end

--- front-end/src/components/ReviewForm/DateFormatters.js
+++
@@ -1,7 +0,0 @@
-export const toPartialISOString = (date) => {
-    const yyyymmdd = new Date(date.getTime() - (date.getTimezoneOffset() * 60000 ))
-        .toISOString()
-        .split("T")[0]
-
-    return yyyymmdd;
-};
